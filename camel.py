e326f460611a6a8c98de38d23ae50272fdbde365
oscarhampart <oh230@kent.ac.uk>
2026-02-19 08:22:51 +0000

    snakeCamelCase

--- camel.py
+++ camel.py
@@ -1,1 +1,11 @@
-# Enter your code here
+camel = input("Enter in camelCase: ")
+
+snake = ""
+
+for char in camel:
+    if char.isupper():
+        snake += "_" + char.lower()
+    else:
+        snake += char 
+
+print("Here it is in snake_case: ", snake)
